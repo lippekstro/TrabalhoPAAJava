1f0a677860a4a0c8df36e1b1e41a421fd78d5907
Luis Fellipe Castro Silva <fellipecastro13@hotmail.com>
2019-03-27 15:47:03 -0300

    o codigo dessa forma resolve tudo de P1.in em 9 Minutos 46seguntos

--- src/HeapSortArray.java
+++ src/HeapSortArray.java
@@ -1,24 +1,20 @@
-
 public class HeapSortArray {
 
-	private int tamanho;
-	
+	//private int tamanho;
 	public void montaHeap(Integer[] array, int tamanho) {
-		//tamanho = array.length - 1;
-		
+		tamanho -= 1;
 		for(int i = tamanho/2; i >= 0; i-- ) {
-			heapify(array, i);
+		heapify(array, i, tamanho);
 		}
 	}
-	
+
 	public void troca(Integer[] array, int x, int y) {
 		int aux = array[x];
 		array[x] = array[y];
 		array[y] = aux;
 	}
-	
-	public void heapify(Integer[] array, int i) {
-		
+
+	public void heapify(Integer[] array, int i, int tamanho) {
 		int inicio = 2 * i;
 		int fim = 2 * i + 1;
 		int maior = i;
@@ -26,41 +22,14 @@ public class HeapSortArray {
 		if(inicio <= tamanho && array[inicio] > array[i]) {
 			maior = inicio;
 		}
-		
+	
 		if(fim <= tamanho && array[fim] > array[maior]) {
 			maior = fim;
 		}
-		
+	
 		if(maior != i) {
 			troca(array, i, maior);
-			heapify(array, maior);
+			heapify(array, maior, tamanho);
 		}
 	}
-	
-//	public void executaHS(Integer[] array) {
-//		montaHeap(array, );
-//		
-//		for(int i = array.length-1; i > 0; i--) {
-//			troca(array, 0, i);
-//			tamanho--;
-//			//mostraArray(array);
-//			heapify(array, 0);
-//		}
-//		//mostraArray(array);
-//	}
-//	
-//	public void mostraArray(Integer[] array) {
-//		for(int i = 0; i < array.length; i++) {
-//			System.out.print(array[i] + " ");
-//		}
-//	}
-//	
-//	//remove elemento
-//	public int removeMaior(Integer[] array) {
-//		int removido = array[0];
-//		array[0] = array[tamanho--];
-//		heapify(array, 0);
-//		return removido;
-//	}
-	
 }
--- src/TesteAlocacaoNaoDinamica.java
+++ src/TesteAlocacaoNaoDinamica.java
@@ -2,52 +2,53 @@ import java.util.Scanner;
 
 public class TesteAlocacaoNaoDinamica {
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		Integer[] array = new Integer[200000];
-		int tamanhoUsado = 0;
-		HeapSortArray hp = new HeapSortArray();
-		int caso = 1;
-		
-		while(caso <= 30) {
-			String itemDeEntada = scan.nextLine().toUpperCase(); //le entrada
-			
-			if(itemDeEntada.contains("I") || itemDeEntada.contains("P")) { //verifica se tem I ou P
-				if(itemDeEntada.contains("I")) { //se tem I
-					array[tamanhoUsado] = Integer.parseInt(itemDeEntada.substring(2)); //insere
-					tamanhoUsado++;
-				} else if (itemDeEntada.contains("P")) { //se tem P
-					if(tamanhoUsado == 0) {
-						System.out.println("empty"); // se nao tiver elementos printa empty
-					} else { //se tiver elementos
-						hp.montaHeap(array, tamanhoUsado);
-						System.out.println(array[0]); // printa o maior
-						int aux = array[tamanhoUsado - 1];
-						array[tamanhoUsado - 1] = array[0];
-						array[0] = aux;
-						tamanhoUsado--;
-						
-					}
+public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	Integer[] array = new Integer[200000];
+	int tamanhoUsado = 0;
+	HeapSortArray hp = new HeapSortArray();
+	int caso = 1;
+
+	while(true) {
+		String itemDeEntada = scan.nextLine().toUpperCase(); //le entrada
+		if(itemDeEntada.contains("I") || itemDeEntada.contains("P")) { //verifica se tem I ou P
+			if(itemDeEntada.contains("I")) { //se tem I
+				array[tamanhoUsado] = Integer.parseInt(itemDeEntada.substring(2)); //insere
+				tamanhoUsado++;
+			} else if (itemDeEntada.contains("P")) { //se tem P
+				if(tamanhoUsado == 0) {
+					System.out.println("empty"); // se nao tiver elementos printa empty
+				} else { //se tiver elementos
+					hp.montaHeap(array, tamanhoUsado);
+					System.out.println(array[0]); // printa o maior
+					//troca o primeiro com o ultimo
+					hp.troca(array, 0, tamanhoUsado - 1);
+					int aux = array[tamanhoUsado - 1];
+					array[tamanhoUsado - 1] = array[0];
+					array[0] = aux;
+					//remove diminuindo o tamanho do array
+					tamanhoUsado--;
 				}
+			}
+		} else {
+			if(itemDeEntada.matches("0 0")) { //verifica se eh uma entrada 0 0 (nao deve fazer nada)
+				System.out.println(); //pulei uma linha pr ficar mais visivel
 			} else {
-				if(itemDeEntada.matches("0 0")) { //verifica se eh uma entrada 0 0 (nao deve fazer nada)
-					System.out.println();
-				} else {
-					if(itemDeEntada.matches("[0-9]* ")) { //se for uma entrada no formato numero|espaço  (pra que nao se confunda com o caso numero|espaco|numero pois era o que estava acontecendo em alguns casos)
-						array[tamanhoUsado] = Integer.parseInt(itemDeEntada.trim()); //pega so o numero e adiciona ao vetor
-						tamanhoUsado++;
-					}
-					else if (itemDeEntada.matches("([0-9]* [0-9]*)")) { //se forem 2 numeros no formato numero|espaco|numero marca o inicio de um novo caso
-						tamanhoUsado = 0;
-						System.out.println("Caso #" + caso);
-						caso++;
-					} 
-					else { //se for apenas um numero inteiro solto entao adiciona no vetor (valores iniciais do vetor)
-						array[tamanhoUsado] = Integer.parseInt(itemDeEntada.trim());
-						tamanhoUsado++;
-					}
+				if(itemDeEntada.matches("[0-9]* ")) { //se for uma entrada no formato (numero|espaço)  (pra que nao se confunda com o caso (numero|espaco|numero) pois era o que estava acontecendo em alguns casos)
+					array[tamanhoUsado] = Integer.parseInt(itemDeEntada.trim()); //pega so o numero e adiciona ao vetor
+					tamanhoUsado++;
+				}
+				else if (itemDeEntada.matches("([0-9]* [0-9]*)")) { //se forem 2 numeros no formato (numero|espaco|numero) marca o inicio de um novo caso
+					tamanhoUsado = 0; // reinicia o array
+					System.out.println("Caso #" + caso);
+					caso++;
+				} 
+				else { //se for apenas um numero inteiro solto entao adiciona no vetor (valores iniciais do vetor)
+					array[tamanhoUsado] = Integer.parseInt(itemDeEntada.trim());
+					tamanhoUsado++;
 				}
 			}
 		}
+	}
 	}
 }
